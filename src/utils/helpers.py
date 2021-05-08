c6f4ce631a146d7f4c55c39ba4b34b78a263fe1a
sitar777 <k.ivkinwot@gmail.com>
2021-05-08 17:00:40 +0300

    added type checking

--- src/utils/helpers.py
+++ src/utils/helpers.py
@@ -1,15 +1,24 @@
+from typing import List
+
 from src.utils.equations import probability_derivative
 from src.config import STEP_HEIGHT
 
 
-def try_get_coeff(coeffs, idx):
+def try_get_coeff(
+    coeffs: List[float],
+    idx: int,
+        ):
     try:
         return coeffs[idx]
     except IndexError:
         return 0
 
 
-def validate_coeffs(coeffs=None, idx=None, denominator=1):
+def validate_coeffs(
+    coeffs: List[float] = None,
+    idx: int = None,
+    denominator: int = 1
+        ):
     if not coeffs:
         return [0, 0, 0]
     if idx is None:
@@ -21,7 +30,13 @@ def validate_coeffs(coeffs=None, idx=None, denominator=1):
     ]
 
 
-def build_rk_coeffs(proba_matrix, time, values_range, coeffs=None, denominator=1):
+def build_rk_coeffs(
+    proba_matrix: List[List[float]],
+    time: int,
+    values_range: int,
+    coeffs: List[float] = None,
+    denominator: int = 1
+        ):
     result = []
     for idx in range(values_range):
         result.append(
